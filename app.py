53a92a452940dccdfc0486d0fda97323f68600ac
Kiri Choi <kirichoi@uw.edu>
2014-05-22 11:33:51 -0700

    Updated paths to get basic example working

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, request, redirect
 import binascii
 import base64
 import ipythonify 
+import os, sys
+
 app = Flask(__name__)
 
+home = os.path.dirname(sys.executable)
+usrdir = os.path.expanduser('~')
+dirname = os.path.join(usrdir, 'intPDF')
 
 @app.route('/')
 def hello_world():
@@ -12,12 +17,12 @@ def hello_world():
 @app.route('/redirect', methods=['GET', 'POST'])
 def make_redirect_ipython():
     archive = request.args.get('archive')
-    ipythonify.pyprep(archive, '/home/user/asdf', 'asdf')
-    notebook = ipythonify.jsonify('/home/user/asdf/asdf.py', '/home/user/asdf/asdf.ipynb') 
-    with open("/home/user/asdf/asdf.ipynb", "w") as notebook_file:
+    ipythonify.pyprep(archive, dirname, 'a')
+    notebook = ipythonify.jsonify(os.path.join(dirname,'a.py'), 'a.ipynb')
+    with open(os.path.join(dirname,'a.ipynb'), "w") as notebook_file:
         notebook_file.write(notebook)
 
-    return redirect('http://localhost:8888', code=302)
+    return redirect('http://localhost:8888/notebooks/' + 'a.ipynb', code=302)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
