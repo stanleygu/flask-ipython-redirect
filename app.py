998e8116b8af7cd85f13982a5f602dddb918c31b
Stanley Gu <stanleygu@gmail.com>
2014-05-22 01:38:08 +0000

    Successfully using ipythonify

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect
 import binascii
 import base64
+import ipythonify 
 app = Flask(__name__)
 
 
@@ -8,26 +9,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-
-def decodeString(s):
-    try:
-        return binascii.unhexlify(s)
-    except TypeError:
-        pass
-    try:
-        return base64.standard_b64decode(s)
-    except TypeError:
-        raise TypeError('Could not decode as hex or base64')
-
-
 @app.route('/redirect', methods=['GET', 'POST'])
 def make_redirect_ipython():
-    # Check if archive is in hex or base64
     archive = request.args.get('archive')
-    decoded = decodeString(archive)
-    print decoded
+    ipythonify.pyprep(archive, '/home/user/asdf', 'asdf')
+    notebook = ipythonify.jsonify('/home/user/asdf/asdf.py', '/home/user/asdf/asdf.ipynb') 
+    with open("/home/user/asdf/asdf.ipynb", "w") as notebook_file:
+        notebook_file.write(notebook)
 
-    return redirect('http://www.google.com', code=302)
+    return redirect('http://localhost:8888', code=302)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
