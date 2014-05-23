cfdeec0c584e2f40804b48041212221449c7a253
Kiri Choi <kirichoi@uw.edu>
2014-05-23 12:43:02 -0700

    Enabled linebreaks

--- encodestr.py
+++ encodestr.py
@@ -68,11 +68,11 @@ def encodestr(output):
     else:
         print "Wrong input"
         strform = encodetype()
-    #outstr = instnl(enstr)
+    outstr = instnl(enstr)
 
     fout = open(outputloc, "wb")
     temp = Template("http://localhost:5000/redirect?title=$name&format=$formtype&archive=$code")
-    fstr = temp.substitute(name = fname, formtype = entype, code = enstr)
+    fstr = temp.substitute(name = fname, formtype = entype, code = outstr)
     fout.write(fstr)
     fin.close()
     fout.close()
